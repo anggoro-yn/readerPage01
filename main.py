e5f3223c461599eedc9f941caec9ecf10606b147
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-20 11:09:07 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+teks01 = '''\
+The fourth and present era (1994 onwards) shows an unprecedented availability of and accessibility to \
+information. With the advent of the Internet and its spread across the globe, the project management \
+community has become more efficient in controlling and managing the various aspects of projects.
+ 
+The first time that the general public in its entirety was confronted with project management and its \
+side effects was around the transition into the new millennium, also known as the Y2K project or the \
+Millennium Bug. The Y2K project was instigated by the belief that on January 1st, 2000, most computer \
+programmes would not function correctly anymore. Many organisations  adopted project management tools \
+and techniques to execute their own Y2K project.
+ 
+In this same period, the oil and gas industry started developing even larger major capital investment \
+projects in order to keep up with the ever growing energy demand of the world. Easy oil has long been \
+discovered and the explorations and developments must now take place in new and unknown territories. \
+Projects are becoming more complex with even bigger technological challenges. Examples include deep-water \
+exploration and production near Brazil, in the Gulf of Mexico and Malaysia (~ 3 km of water depth), \
+ever larger Liquid Natural Gas plants in the Middle East and the far east of Russia (liquefying \
+natural gas at low temperature and transporting it by ship) and the Gas to Liquid plants in South Africa \
+and in Qatar (making liquid products out of gas via the Fischer-Tropsch process). 1.3 From ‘over the fence’ \
+management to project management Over-the-fence management (Kerzner, 2005) preceded contemporary project management. \
+Traditionally, each discipline or function took care of its own activities. When the task was completed, \
+the job was given to the next function in the line (‘thrown over the fence’). This means  \
+that all the work was completely sequentially in nature and the last in line would most probably get \
+all the blame if the project went off-track. As a consequence schedule improvement was hard to realise. \
+The majority of project activities were performed by line managers who were more interested in the advancement \
+of their own departments than the success of the task at hand. In fact, nobody was truly looking after \
+the best interest of the project. Project management did not really exist. Another disadvantage of this \
+sequential approach was that it was very difficult for customers to keep track of where their ‘project’ was. \
+Therefore at a certain stage, customers (and looking at
+ the earlier examples this was most often the government) demanded a single point of contact throughout the \
+ ‘project’, a person who would be part of the development process during the  entire lifecycle. When this \
+ happened in the 1950s, the project manager was born. Initially, this came with much resistance from senior \
+ managers as well as sales and marketing staff, since all  were afraid of losing their influence. From there \
+ on the whole project management approach has been mostly based on best practice sharing and handing over of \
+ experience from manager to manager. This system has quite  a number of similarities with the traditional \
+ medieval master/fellow relation, although project management is a much younger discipline.
+'''
+st.write(teks01)
